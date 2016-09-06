784e8e1ce6e253cf104d335e50338dc71690db02
your name <petleya@onid.oregonstate.edu>
2016-09-06 11:19:53 -0700

    dispaying data in table
    able to query data and render in html template. Right now it is hard
    coded to client A. Working on linking this to client drop down list

--- BCproject/bcp.py
+++ BCproject/bcp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\AvaJ84\\Downloads\\sqlite-3_10_1_win\\sqlite-3_10_1\\BCproject'
@@ -9,7 +10,6 @@ db = SQLAlchemy(app)
 
 class FRequest(db.Model):
     __tablename__ = 'f_request'
-##    id = db.Column('id', db.Integer, primary_key=True)
     title = db.Column('title', db.Unicode, primary_key=True)
     description = db.Column('description', db.Unicode)
     client = db.Column('client', db.Unicode)
@@ -19,7 +19,6 @@ class FRequest(db.Model):
     pArea = db.Column('pArea', db.Unicode)
 
     def __init__(self, title, description, client, priority, date, url, pArea):
-##        self.id = id
         self.title = title
         self.description = description
         self.client = client
@@ -36,7 +35,10 @@ class FRequest(db.Model):
 
 @app.route('/')
 def base_page():
-    return render_template("basePage.html")
+    val = 'hello this is some text'
+    fr = db.session.query(FRequest).all()
+    value = fr
+    return render_template("basePage.html", vals=value)
 
 
 @app.route('/dblink2', methods=['GET', 'POST'])
@@ -45,7 +47,15 @@ def enter_data():
                            request.form['client'], request.form['priority'], request.form['date'], request.form['url'], request.form['pArea'])
     db.session.add(new_request)
     db.session.commit()
-    return " no problems"
+#    try:
+       ## fr = FRequest.query.order_by(FRequest.title)
+       ## value = fr.title
+ #   except:
+#        value = "not working"
+    fr = db.session.query(FRequest).all()
+  #  fr = FRequest.query.all()
+    value = fr[1].description
+    return value
 
 
 
